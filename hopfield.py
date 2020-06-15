723a09ce14f10322f87c77d08502571d4665ac59
Kusaka <popy73hongeop@gmail.com>
2020-06-16 08:49:33 +0900

    Add calculation of performance

--- hopfield.py
+++ hopfield.py
@@ -23,25 +23,42 @@ class Hopfield:
         """calculate potential energy"""
         return - np.dot(np.dot(self.W, x), x) / 2 + np.sum(self.theta * x)
 
-    def update(self, data):
+    def update(self, data, train_data, trial):
+        sim_list = []
+        acc_list = []
         for i in range(self.q):
-            v0 = self.energy(data[i])
-            #print(v0)
-            for j in range(5):
+            #v = self.energy(data[i])
+            #print(v)
+            sim_ave = 0
+            acc_ratio = 0
+            for j in range(trial):
                 data[i] = np.sign(np.dot(self.W, data[i]) - self.theta)
                 #print("updating")
                 #print(data)
+
+                #calculate recall performance
+                sim, acc = self.performance(data[i], train_data[i])
+                sim_ave += sim / trial
+                acc_ratio += acc / trial
+            sim_list.append(sim_ave)
+            acc_list.append(acc_ratio)
+
             """
             while True:
+                cnt += 1
+                v0 = self.energy(data[i])
                 data[i] = np.sign(np.dot(self.W, data[i]) - self.theta)
-                v1 = energy(data[i])
+                v1 = self.energy(data[i])
                 print("now recollecting")
+
+                acc, ratio = self.performance(data[i], train_data[i])
+                acc_list.append(acc)
+
                 if v0 == v1:
-                    updated[i] = data[i]
                     break
             """
             #self.visualize(data)
-        return data
+        return data, sim_list, acc_list
 
     def visualize(self, data):
         for i in range(self.q):
@@ -58,14 +75,28 @@ class Hopfield:
         #print("Add noise")
         return data
 
+    def performance(self, data, train_data):
+        sim = 0
+        acc = 0
+        for i in range(self.size):
+            if data[i] == train_data[i]:
+                sim += 1
+            sim = sim / self.size
+            
+        if (data == train_data).all():
+            acc += 1
+        return sim, acc
+        
+
 def train(train_data):
     hopfield = Hopfield(train_data)
     hopfield.visualize(train_data)
     init = hopfield.noise(train_data, 0.10)
     hopfield.visualize(init)
-    recollected = hopfield.update(init)
+    recollected , sim, acc = hopfield.update(init, train_data, 5)
     hopfield.visualize(recollected)
-    #print("recollected")
+    print("similarity : {}, accuracy : {}".format(sim, acc))
+    #print("recalled")
 
 if __name__ == "__main__":
     train_data = np.array([np.array([-1, -1, 1, -1, -1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, -1]),
